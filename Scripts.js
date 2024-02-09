bd4326978d9e97424a3124d72964417ede2b35c6
Jdaviddddddk <jhonnatana06@gmail.com>
2024-02-09 09:13:42 -0500

    Commit General 1

---
+++ Scripts.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//DECLARADOR DE VARIABLES
+const tPort = document.querySelector(".prt");
+const ePort = document.querySelector(".hidden1");
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    scrub: 1,
+    //markers: true,
+    pin: true,
+    trigger: ".fixedPort",
+    start: "0% 0%",
+    end: "120% 0%",
+  },
+});
+
+tl.to(tPort, {
+    scale: 0.5,
+    y: -200,
+    duration: 4,
+});
+
+tl.to(".hidden1", {
+    opacity: "100%",
+    duration: 4,
+});
