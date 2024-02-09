c7a19468c12594400aaa600e72aefc568c5ef732
Jdaviddddddk <137846418+Jdaviddddddk@users.noreply.github.com>
2024-02-09 11:30:15 -0500

    c

--- Scripts.js
+++ Scripts.js
@@ -29,7 +29,7 @@ tl.to(".hidden1", {
 const tl1 = gsap.timeline({
   scrollTrigger: {
     scrub: 1,
-    //markers: true,
+    markers: true,
     pin: true,
     trigger: ".model",
     start: "0% 0%",
@@ -38,6 +38,6 @@ const tl1 = gsap.timeline({
 });
 
 tl1.to(".model", {
-  RotateY: "90deg",
-  duration: 4,
+  rotation: 360,
+  duration: 2,
 });
